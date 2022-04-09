2a31129f548cb0a1f1dbee4ed8133265ce945eb1
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-04-09 12:07:22 +0100

    add text

--- CALL-me-MT/src/app/app.component.ts
+++ CALL-me-MT/src/app/app.component.ts
@@ -17,7 +17,41 @@ export class AppComponent {
       console.log(res);
     });
     
+    let textarea = <HTMLInputElement>document.getElementById("readingMaterial");
+    textarea.value = "hello"
+    
+    let spanishData = await this.http.get('../assets/spanishText.txt').toPromise()!;
+    
+    /*
+    const myArea = <HTMLInputElement>document.getElementById("readingMaterial")
+    if(myArea) {
+      myArea.value = "Hello world!"
+      myArea.onclick = (e) => {
+        if(e.currentTarget) {
+          let i = e.currentTarget.selectionStart
+          console.log(this.getTheWord(i, myArea.value))
+          
+        }
+      
+      }
+      
+    }
+    */
+
+    
   }
+  
+  getTheWord(selectionStart:any, value:any){
+    let arr = value.split(" ");
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i].length + 1
+      if (sum > selectionStart) return arr[i]
+
+    }
+  }
+
+
   
   
 }
